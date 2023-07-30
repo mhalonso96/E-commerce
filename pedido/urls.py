a33480cae34efd711cd5d3264d22008740730b1f
mhalonso96 <celo.alonso@gmail.com>
2023-07-30 08:57:48 -0300

    🔒️

--- pedido/urls.py
+++ pedido/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'pedido'
 
 urlpatterns = [
-    path('pagar/', views.Pagar.as_view(), name= 'pagar'),
+    path('', views.Pagar.as_view(), name= 'pagar'),
     path('fecharpedido/', views.FecharPedido.as_view(), name= 'fecharpedido'),
     path('detalhe/', views.Detalhe.as_view(), name= 'detalhe'),
 

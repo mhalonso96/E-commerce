2f540ea12e459a619dfe592d42ac7547f09ff4c7
mhalonso96 <celo.alonso@gmail.com>
2023-07-31 16:44:10 -0300

    💄 adicionando static e templates

--- produto/views.py
+++ produto/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
-
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('ListaProduto')
+from . import models
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
 class DetalheProduto(View):
     def get(self, *args, **kwargs):
         return HttpResponse('DetalheProduto')
